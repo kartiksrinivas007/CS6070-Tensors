9da995e1a2ff7d68eaf24d3700c1b5d2ad055b7c
tyagio <cs20btech11060@iith.ac.in>
2023-04-20 19:16:55 +0530

    Making kruskal safer

--- tensor/operation/kruskal.py
+++ tensor/operation/kruskal.py
@@ -1,4 +1,10 @@
 import numpy as np
+import string
+
+def nary_outer_einsum_52(*vectors):
+    subscripts = string.ascii_letters[:len(vectors)]
+    subscripts = ','.join(subscripts) + '->' + subscripts
+    return np.einsum(subscripts, *vectors)
 
 def kruskal(*args):
     """kruskal computes the outer product of a list of matrices and sums them.
@@ -14,7 +20,8 @@ def kruskal(*args):
     R = args[0].shape[1]
     out = np.zeros(tuple([A.shape[0] for A in args]))
     for i in range(R):
-        out += np.prod(np.ix_(*[A[:, i] for A in args]))
+    #     out += np.prod(np.ix_(*[A[:, i] for A in args]))
+        out+=nary_outer_einsum_52(*[A[:, i] for A in args])
 
     return out
 
